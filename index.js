745cf655bfa4044da63ea5e9e7749d8f63da546a
Malcolm Keyes <3.malcolmk@gmail.com>
2022-07-11 12:03:38 -0400

    commit tree 81401a2f73bf87a021bd26ae1e357b805f84319c

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let hour = parseInt(timeString.split(':')[0]);
+
+  if (hour < 12) {
+    return 'Good Morning';
+  } else if (hour < 18) {
+    return 'Good Afternoon';
+  } else {
+    return 'Good Evening';
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  document.getElementById('greeting').innerHTML = message;
+}
